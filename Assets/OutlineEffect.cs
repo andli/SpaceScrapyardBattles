98fd3dc55c5404823e8df31912f7358c45408921
Andreas Ehrlund <a.ehrlund@gmail.com>
2020-01-26 02:09:25 +0100

    WIP

---
+++ Assets/OutlineEffect.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineEffect : MonoBehaviour
+{
+    [SerializeField] private Material material;
+
+    private float outlineThickness;
+    private float fadeSpeed = 1f;
+    private bool isOutlined;
+
+    private void Update()
+    {
+        if (isOutlined)
+        {
+            outlineThickness = Mathf.Clamp01(outlineThickness + fadeSpeed * Time.deltaTime);
+            material.SetFloat("_OutlineThickness", outlineThickness);
+
+        }
+        else
+        {
+            outlineThickness = Mathf.Clamp01(outlineThickness - fadeSpeed * Time.deltaTime);
+            material.SetFloat("_OutlineThickness", outlineThickness);
+
+        }
+
+    }
+
+    public void StartOutlining()
+    {
+        Debug.Log("Outline start");
+        if (!isOutlined)
+        {
+            isOutlined = true;
+
+        }
+    }
+    public void StopOutlining()
+    {
+        Debug.Log("Outline STOP");
+        if (isOutlined)
+        {
+            isOutlined = false;
+
+        }
+    }
+}
---
+++ Assets/dragGameSprite.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class dragGameSprite : MonoBehaviour
+{
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    void OnMouseDown()
+    {
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        GetComponent<ShipPartDisplay>().setDragging(true);
+
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
+    }
+
+    void OnMouseUp()
+    {
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        GetComponent<ShipPartDisplay>().setDragging(false);
+
+    }
+}
