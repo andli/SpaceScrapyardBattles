744951af56c10bcdec7bf8cdc273a370a0cbdd19
Andreas Ehrlund <a.ehrlund@gmail.com>
2020-01-26 02:08:47 +0100

    WIP

--- Assets/ShipPartDisplay.cs
+++ Assets/ShipPartDisplay.cs
@@ -7,35 +7,64 @@ public class ShipPartDisplay : MonoBehaviour
 {
     public ShipPart shipPart;
 
-    public Image artwork;
-    public Image background;
+    private float rotationCooldown = 0.2f;
+    private float keyTimestamp;
+    private bool beingDragged = false;
 
-    private Material outlineMaterial;
+    private void FixedUpdate()
+    {
+        if (beingDragged && Input.GetKey(KeyCode.R))
+        {
+            if (keyTimestamp + rotationCooldown < Time.time)
+            {
+                this.rotate90CW();
+                keyTimestamp = Time.time;
+            }
+        }
+    }
 
     public void rotate90CW()
     {
-        artwork.transform.Rotate(new Vector3(0, 0, 90));
-        shipPart.rotation = artwork.transform.rotation.eulerAngles;
+        Debug.Log("rotate!");
+        SpriteRenderer[] rs = GetComponentsInChildren<SpriteRenderer>(true);
+        rs[1].transform.Rotate(new Vector3(0, 0, 90));
+
+        shipPart.rotation = rs[1].transform.rotation.eulerAngles;
+    }
+
+    public void setDragging(bool isDragging)
+    {
+        this.beingDragged = isDragging;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("TEST COLLIDE");
-
-        artwork.material = outlineMaterial;
+        OutlineEffect outlineEffect = GetComponent<OutlineEffect>();
+        outlineEffect.StartOutlining();
     }
 
-    private void Awake()
+    private void OnTriggerExit2D(Collider2D collision)
     {
-         outlineMaterial = Resources.Load<Material>("OutlineMaterial");
+        OutlineEffect outlineEffect = GetComponent<OutlineEffect>();
+        outlineEffect.StopOutlining();
     }
+
     // Start is called before the first frame update
     void Start()
     {
-        artwork.sprite = shipPart.artwork;
+        SpriteRenderer[] rs = GetComponentsInChildren<SpriteRenderer>(true);
+     
+        foreach (SpriteRenderer spriteRenderer in rs)
+        {
+            if (spriteRenderer.name == "Artwork")
+            {
+                spriteRenderer.sprite = shipPart.artwork;
+            }
+        }
     }
     void Update()
     {
+
     }
 
 
